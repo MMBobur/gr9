14df570136426b92985ba658146c41d4433270da
Asrorjon17 <asrorjonrahimjonov17@gmail.com>
2022-06-02 13:22:25 +0500

    asror

--- src/App.js
+++ src/App.js
@@ -5,12 +5,7 @@ import { Box, Grid } from "@mui/material";
 function App() {
 	return (
 		<div style={{ display: "flex" }}>
-			<Grid container>
-				<Grid item lg={2.5} md={3} sm={0} xs={0}></Grid>
-				<Grid item lg={9.5} md={9} sm={12} xs={12}>
-					<Checkout />
-				</Grid>
-			</Grid>
+			<Checkout />
 		</div>
 	);
 }
